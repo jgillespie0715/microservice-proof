6311083c85d7a7d37c31de13427392b88820e47b
Justin Gillespie <jgillespie0715@gmail.com>
2021-02-05 00:06:01 -0500

    Refactor: PostList fetch Query svc
    reduce network calls, keep app
    in sync
    
    *CommentList render on conditions
    of status approved, moderation,
    or rejection

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,24 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 
-export default ({ postId }) => {
-	const [comments, setComments] = useState([]);
-
-	const fetchData = async () => {
-		const res = await axios.get(
-			`http://localhost:4001/posts/${postId}/comments`
-		);
-
-		setComments(res.data);
-	};
-
-	useEffect(() => {
-		fetchData();
-		// eslint-disable-next-line
-	}, []);
-
+export default ({ comments }) => {
 	const renderComments = comments.map((comment) => {
-		return <li key={comment.id}>{comment.content}</li>;
+		let content;
+		if (comment.status === 'approved') {
+			content = comment.content;
+		}
+		if (comment.status === 'pending') {
+			content = 'This comment is awaiting moderation';
+		}
+		if (comment.status === 'rejected') {
+			content = 'This comment has been rejected';
+		}
+
+		return <li key={comment.id}>{content}</li>;
 	});
 	return <ul>{renderComments}</ul>;
 };
